c5a8959cb1b5ee7df635585032f77edfdcd3d6bd
root <acacio.am@gmail.com>
2021-02-14 19:27:21 -0300

    AAM - Ajuste issues principal

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -39,7 +39,7 @@
 <body>
 <nav id="header" class="navbar navbar-expand-md navbar-dark">
     <div id="header-logo" class="navbar-brand">
-        <img src="<?= base_url('assets/img/logo.png') ?>">
+        <img src="<?= base_assets_url('assets/img/logo.png') ?>">
         <h6>EASY!APPOINTMENTS</h6>
         <small>Open Source Appointment Scheduler</small>
     </div>
